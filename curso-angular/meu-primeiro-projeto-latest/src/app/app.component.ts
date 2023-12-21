a004e61c37d85638ef1883dd6476c80e5babb9e5
Aguinaldo Neto <neto_aguinaldo@outlook.com>
2023-12-20 22:11:07 -0300

    Meu primeiro commit

---
+++ curso-angular/meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NewComponentComponent } from './components/new-component/new-component.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, NewComponentComponent],
+  template: `
+    <router-outlet></router-outlet>
+
+    <h1>Curso de Angular</h1>
+    <app-new-component/>
+    `,
+})
+export class AppComponent {
+  title = 'meu-primeiro-projeto-latest';
+}
